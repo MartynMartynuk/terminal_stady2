17307986caa09788b2293bac68a93e97279d0a24
Martyn <aa>
2022-01-21 19:57:16 +0400

    Refactored bitlink detection function

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import os
 from dotenv import load_dotenv
 
 
-BITLY_TOKEN = os.getenv('BITLY_TOKEN')
+
 
 
 def get_incoming_url():
@@ -14,11 +14,11 @@ def get_incoming_url():
         response.raise_for_status()
         return long_url
     except:
-        return "Неверная ссылка\n", "Ошибка! Код ответа:", \
-               response.status_code
+        print("Неверная ссылка!")
+        exit()
 
 
-def shorten_link(long_url, token=BITLY_TOKEN):
+def shorten_link(long_url, token):
     url = "https://api-ssl.bitly.com/v4/shorten"
     headers = {"Authorization": f"Bearer {token}"}
     body = {"long_url": long_url}
@@ -27,7 +27,7 @@ def shorten_link(long_url, token=BITLY_TOKEN):
     return response.json()["id"]
 
 
-def count_clicks(bitly_id, token=BITLY_TOKEN):
+def count_clicks(bitly_id, token):
     headers = {"Authorization": f"Bearer {token}"}
     info_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitly_id}/clicks/summary"
     response = requests.get(info_url, headers=headers)
@@ -35,22 +35,31 @@ def count_clicks(bitly_id, token=BITLY_TOKEN):
     return response.json()["total_clicks"]
 
 
-def is_bitlink(url):
-    if urllib.parse.urlparse(url).netloc == "bit.ly":
+def is_bitlink(url, token):
+    try:
+        headers = {"Authorization": f"Bearer {token}"}
+        bitlink = urllib.parse.urlparse(url).netloc + \
+                  urllib.parse.urlparse(url).path
+        info_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}"
+        response = requests.get(info_url, headers=headers)
+        response.raise_for_status()
         return True
-    else:
+    except:
         return False
+
 
 
 if __name__ == "__main__":
     load_dotenv()
+    BITLY_TOKEN = os.getenv('BITLY_TOKEN')
+
     incom_url = get_incoming_url()
     bitlink_detector = is_bitlink(incom_url)
     if bitlink_detector is True:
         bitlink_id = urllib.parse.urlparse(incom_url).netloc + \
                    urllib.parse.urlparse(incom_url).path
-        result = count_clicks(bitlink_id)
+        result = count_clicks(bitlink_id, BITLY_TOKEN)
         print("Количество переходов по ссылке: ", result)
     else:
-        result = shorten_link(incom_url)
+        result = shorten_link(incom_url, BITLY_TOKEN)
         print("Битлинк: ", result)
