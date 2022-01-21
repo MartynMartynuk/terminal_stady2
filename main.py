76e62dde49a94c3738092fdca0df9776ce068c44
Martyn <aa>
2022-01-21 16:53:56 +0400

    Refactored program

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 BITLY_TOKEN = os.getenv('BITLY_TOKEN')
 
 
-def input_func():
+def get_incoming_url():
     try:
         long_url = input('Введите ссылку: ')
         test_response = requests.get(long_url)
@@ -24,7 +24,7 @@ def shorten_link(long_url, token=BITLY_TOKEN):
     body = {"long_url": long_url}
     response = requests.post(url, headers=headers, json=body)
     response.raise_for_status()
-    return "Битлинк", response.json()["id"]
+    return response.json()["id"]
 
 
 def count_clicks(bitly_id, token=BITLY_TOKEN):
@@ -32,19 +32,25 @@ def count_clicks(bitly_id, token=BITLY_TOKEN):
     info_url = f"https://api-ssl.bitly.com/v4/bitlinks/{bitly_id}/clicks/summary"
     response = requests.get(info_url, headers=headers)
     response.raise_for_status()
-    return "Количество переходов по ссылке", response.json()["total_clicks"]
+    return response.json()["total_clicks"]
 
 
 def is_bitlink(url):
     if urllib.parse.urlparse(url).netloc == "bit.ly":
-        bitly_id = urllib.parse.urlparse(url).netloc +\
-                   urllib.parse.urlparse(url).path
-        return count_clicks(bitly_id)
+        return True
     else:
-        return shorten_link(url)
+        return False
 
 
 if __name__ == "__main__":
     load_dotenv()
-    result = is_bitlink(input_func())
-    print(f"{result[0]}: {result[1]}")
+    incom_url = get_incoming_url()
+    bitlink_detector = is_bitlink(incom_url)
+    if bitlink_detector is True:
+        bitlink_id = urllib.parse.urlparse(incom_url).netloc + \
+                   urllib.parse.urlparse(incom_url).path
+        result = count_clicks(bitlink_id)
+        print("Количество переходов по ссылке: ", result)
+    else:
+        result = shorten_link(incom_url)
+        print("Битлинк: ", result)
